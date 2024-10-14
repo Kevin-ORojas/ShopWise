a662d552bca86ba761cccc7b20193842ce3a2168
NozoDev <kevinroj326@gmail.com>
2024-10-13 20:14:26 -0500

    historial

--- src/components/HistoriPurchase/HistoriPurchase.jsx
+++ src/components/HistoriPurchase/HistoriPurchase.jsx
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
 
 export const HistoriPurchase = ({ historial }) => {
-  const [isVisible, setIsVisible] = useState(false); // Estado para controlar la visibilidad del historial
+  const [isVisible, setIsVisible] = useState(false);
 
-  const toggleVisibility = () => {
-    setIsVisible(!isVisible);
-  };
+  const toggleVisibility = () => setIsVisible(!isVisible);
 
   const calculateTotalPrice = () => {
     return historial.reduce((total, item) => total + item.precio, 0).toFixed(2);
   };
-
   return (
     <div className="max-w-full mx-auto mt-8 p-6 rounded-lg shadow-lg">
       <h2
@@ -20,24 +17,30 @@ export const HistoriPurchase = ({ historial }) => {
       >
         Historial de Compras {isVisible ? "▲" : "▼"}
       </h2>
-      {isVisible && (
-        <>
-          <ul>
-            {historial.length === 0 ? (
-              <p>No hay productos en el historial de compras.</p>
-            ) : (
-              historial.map(({ nombre, precio, date }, index) => (
-                <li key={index}>
-                  <p>
-                    Producto: {nombre}, Precio: ${precio}, Fecha: {date}
-                  </p>
-                </li>
-              ))
-            )}
-          </ul>
-          <h3 className="font-bold">Precio Total: ${calculateTotalPrice()}</h3>
-        </>
-      )}
+      <div className=" mt-4 bg-orange-300">
+        {isVisible && (
+          <div className="flex justify-around">
+            <ul>
+              {historial.length === 0 ? (
+                <p>No hay productos en el historial de compras.</p>
+              ) : (
+                historial.map(({ nombre, precio, date }, index) => (
+                  <div
+                    className="flex gap-8 border-b-2 border-r-2 border-t-2 border-l-2 mt-4"
+                    key={index}
+                  >
+                    <p>Producto: {nombre}</p>
+                    <p>Precio: ${precio}</p>
+                  </div>
+                ))
+              )}
+            </ul>
+            <h3 className="font-bold">
+              Precio Total: ${calculateTotalPrice()}
+            </h3>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
