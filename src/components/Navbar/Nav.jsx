0a9b1b4e24975e8509a280e983053a31534d884f
NozoDev <kevinroj326@gmail.com>
2024-10-18 21:17:50 -0500

    estilos al navbar

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { usePurchaseHistory } from "../../hook/usePurchaseHistory";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export const Nav = () => {
   const [showNav, setShowNav] = useState(false);
@@ -9,53 +9,55 @@ export const Nav = () => {
     setShowNav(!showNav);
   };
   return (
-    <nav className="bg-slate-700 shadow-lg">
-      <button className="md:hidden" onClick={toggleNav}>
-        {showNav ? "Hide menu" : "Show menu"}
+    <nav className="bg-slate-700 shadow-lg py-4 flex">
+      <button
+        className="md:hidden h-20  text-white focus:outline-none"
+        onClick={toggleNav}
+      >
+        <MenuIcon />
       </button>
-      {showNav && (
-        <div
-          className={`absolute top-16 left-0 w-full bg-gray-100 p-4 shadow-md transition-transform duration-300 ${
-            showNav ? "block" : "hidden"
-          }`}
-        >
-          <div className="text-white text-xl font-bold">
-            <p>Logo</p>
+      <div
+        className={` sm:w-full md:flex md:items-center md:space-x-6 transition duration-700 ease-in-out ${
+          showNav ? "block" : "hidden"
+        } transition duration-700 ease-in-out`}
+      >
+        <div className="md:flex md:justify-end w-full px-4 gap-4">
+          <div className="md:flex md:justify-start w-full">
+            {" "}
+            <p className="text-white">Logo</p>
           </div>
-          <div className="flex space-x-6">
-            <Link
-              className="text-white hover:text-slate-300 transition duration-200"
-              to={"/"}
-            >
-              Home
-            </Link>
-            <Link
-              className="text-white hover:text-slate-300 transition duration-200"
-              to={"/products"}
-            >
-              Productos
-            </Link>
-            <Link
-              className="text-white hover:text-slate-300 transition duration-200"
-              to={"/login"}
-            >
-              Login
-            </Link>
-            <Link
-              className="text-white hover:text-slate-300 transition duration-200"
-              to={"/historial"}
-            >
-              Historial
-            </Link>
-            <Link
-              className="text-white hover:text-slate-300 transition duration-200"
-              to={"/register"}
-            >
-              Register
-            </Link>
-          </div>
+          <Link
+            className="text-white hover:text-slate-300 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 p-2 rounded-sm hover:bg-indigo-500 duration-300"
+            to={"/"}
+          >
+            Home
+          </Link>
+          <Link
+            className="text-white hover:text-slate-300 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 p-2 rounded-sm hover:bg-indigo-500 duration-300"
+            to={"/products"}
+          >
+            Productos
+          </Link>
+          <Link
+            className="text-white hover:text-slate-300 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 p-2 rounded-sm hover:bg-indigo-500 duration-300"
+            to={"/login"}
+          >
+            Login
+          </Link>
+          <Link
+            className="text-white hover:text-slate-300 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 p-2 rounded-sm hover:bg-indigo-500 duration-300"
+            to={"/historial"}
+          >
+            Historial
+          </Link>
+          <Link
+            className="text-white hover:text-slate-300 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 p-2 rounded-sm hover:bg-indigo-500 duration-300"
+            to={"/register"}
+          >
+            Register
+          </Link>
         </div>
-      )}
+      </div>
     </nav>
   );
 };
